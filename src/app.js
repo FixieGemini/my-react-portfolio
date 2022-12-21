73a137948e60f305be5dcd49165aa6c1464ad682
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2022-12-21 13:48:43 -0700

    added components to components directory and imported to app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 //Components name and order
 export default function App() {
