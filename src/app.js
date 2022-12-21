beaac0c83efd3a7ef967771390cf7337ea20ebf6
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2022-12-21 13:27:25 -0700

    listed components in app js, created src directory

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Components name and order
+export default function App() {
+    return (
+        <main>
+            <Navbar />
+            <About />
+            <Projects />
+            <Skills />
+            <Contact />
+        </main>
+    );
+}
