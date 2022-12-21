b4f4c07d737f521cacb425cfe26bc9fdd31e0272
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2022-12-21 14:40:28 -0700

    started building out about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,2 +1,38 @@
+import React from "react";
 
-export default function About() {}
+export default function About() {
+    return (
+        <section id="">
+            <div className="">
+                <div className="">
+                    <h1 className="">
+                        Hi, I'm Yari.
+                        <br className="" />I love building amazing apps!
+                    </h1>
+                    <p className="">
+                        My name is Yari, and I am an aspiring full stack web developer.
+                    </p>
+                    <div className="">
+                        <a
+                            href="#contact"
+                            className="">
+                            Work With Me
+                        </a>
+                        <a
+                            href="#projects"
+                            className="">
+                            See My Work
+                        </a>
+                    </div>
+                </div>
+                <div className="">
+                    <img
+                        className=""
+                        alt=""
+                        src=""
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
